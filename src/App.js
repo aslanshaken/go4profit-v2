20735bdb2a8ce204c100280b650235cd9e4de791
asikegoi <aslanshaken@gmail.com>
2021-06-24 12:06:20 -0400

    added fundaments

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Go4Profit</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Share/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+// import Nav from '../header/Header'
+// import Foot from '../footer/Footer'
+
+// export default function Layout(props) {
+//     return (
+//         <div>
+//             <Nav/>
+//             {props.children}
+//             <Foot />
+//         </div>
+//     )
+// }
